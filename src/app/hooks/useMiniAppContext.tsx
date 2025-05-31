9135cd1c5570422d2a8f74e766ea550203c77f4c
Yash Garg <yash431garg@gmail.com>
2025-05-31 18:35:11 +0530

    project initialized

---
+++ src/app/hooks/useMiniAppContext.tsx
@@ -0,0 +1,44 @@
+
+import { useFrame } from "@/app/components/farcaster-proivder";
+import { FrameContext } from "@farcaster/frame-core/dist/context";
+import sdk from "@farcaster/frame-sdk";
+
+// Define specific types for each context
+interface FarcasterContextResult {
+    context: FrameContext;
+    actions: typeof sdk.actions | null;
+    isEthProviderAvailable: boolean;
+}
+
+interface NoContextResult {
+    type: null;
+    context: null;
+    actions: null;
+    isEthProviderAvailable: boolean;
+}
+
+// Union type of all possible results
+type ContextResult = FarcasterContextResult | NoContextResult;
+
+export const useMiniAppContext = (): ContextResult => {
+    // Try to get Farcaster context
+    try {
+        const farcasterContext = useFrame();
+        if (farcasterContext.context) {
+            return {
+                context: farcasterContext.context,
+                actions: farcasterContext.actions,
+                isEthProviderAvailable: farcasterContext.isEthProviderAvailable,
+            } as FarcasterContextResult;
+        }
+    } catch (e) {
+        console.log(e)
+        // Ignore error if not in Farcaster context
+    }
+
+    // No context found
+    return {
+        context: null,
+        actions: null,
+    } as NoContextResult;
+};
